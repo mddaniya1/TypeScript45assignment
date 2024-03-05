fab35f7a418154ea7db4a70e5faee509b24002ea
Md, Daniyal <151908847+mddaniya1@users.noreply.github.com>
2024-03-05 16:03:41 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,5 +2,5 @@
 //Install Node.js, TypeScript and VS Code on your computer
 
 //Q#02:Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
-let eric = "“Hello Eric, would you like to learn some Python today?”";
-console.log(eric);
+let personName = "Eric";
+console.log("Hello ${personName}, would you like to learn some Python today?");
