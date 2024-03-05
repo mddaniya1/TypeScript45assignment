9774d99c533e411d66fd566122d64b7e0c07c814
Md, Daniyal <151908847+mddaniya1@users.noreply.github.com>
2024-03-05 16:26:00 +0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -4,3 +4,17 @@
 //Q#02:Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
 let personName = "Eric";
 console.log("Hello ${personName}, would you like to learn some Python today?");
+
+// Question #03 Name Cases: Store a person’s name in a variable,
+// and then print that person’s name in lowercase, uppercase, and titlecase.
+
+let person = "Daniyal";
+let Inlowercase = person.toLowerCase();
+let InUppercase = person.toUpperCase();
+let titlecasename = person.split('');
+for(let i=0; i< titlecasename.length; i++){
+titlecasename[i] =titlecasename[i].charAt(0).toUpperCase() + titlecasename[i].slice(1).toLowerCase();
+}
+console.log(titlecasename.toString().replace(/,/g , ''));
+console.log("Name in Lowercase:${Inlowercase}");
+console.log("Name in Upperase:${InUppercase}");    
